320f85a801ba610c57ec013473712628b77681c1
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-07-08 15:23:28 -0300

    exercícios referente ao dia 08/07

---
+++ 01-exercicio.rb
@@ -0,0 +1,27 @@
+# quantas horas há em um ano?
+puts '1 ano = 365 dias, e cada dia tem 24 horas'
+puts '365 x 24 = ' + (365 * 24).to_s + ' horas no ano'
+puts 
+
+# quantos minutos há em uma decada?
+puts 'sabemos que um ano tem ' + (365 * 24).to_s + ' horas'
+puts 'e cada hora tem 60 minutos: 60 x 8760 = ' + (60 * 8760).to_s + ' minutos em 1 ano'
+puts 'cada decada é 10 anos, logo 525600 x 10 = ' + (525600 * 10).to_s + ' minutos em uma decada'
+puts
+
+# qual minha idade em segundos?
+puts 'se cada ano tem 525600 minutos e cada minuto 60 segundos, fazemos 525600 x 60 = ' + (525600 * 60).to_s 
+puts 'cada ano tem 31536000 segundos e eu tenho 20 anos de idade'
+puts '31536000 x 20 = ' + (31536000 * 20).to_s + ' segundos de vida (contando...)'
+puts
+
+# quantos chocolates pretendo comer na vida? 
+puts 'não sou muito chegado a chocolate, mas vamos supor que eu coma umas 5 barras de chocolate por mês'
+puts 'cada ano tem 12 meses então eu como 12 x 5 = ' + (12 * 5).to_s + ' barras de chocolates por ano'
+puts 'eu pretendo viver no minímo ate os 80 anos de idade'  
+puts 'então seria 60 x 80 = ' + (60 * 80).to_s + ' barras de chocolate em toda a vida, mais ou menos'
+puts
+
+
+# se você 1232 milhões de segundos, quantos anos você tem?
+puts 'se 1 ano tem 31536000 segundos , é só fazer 1232000000/31536000 = ' + (1232000000/3153600).to_s + ' anos de idade'
