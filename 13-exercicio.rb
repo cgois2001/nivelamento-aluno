1243f45c7178c0e10f96d8a1f4dc4a8cc3c1630c
cgois2001 <carlosdaniel.gois2001@outlook.com>
2021-07-20 12:46:07 -0300

    2º versão do exercicio do dia 20/07

--- 13-exercicio.rb
+++ 13-exercicio.rb
@@ -15,19 +15,32 @@ posicao = gets.to_i
 def remove_da_posicao(array, posicao)
     novo_array = []
     x = array.size
-    if posicao >= x || posicao == 0
+    if posicao >= x || posicao <= 0
         return array
     else 
-        for n in (0..posicao)
-            if n == posicao
-                until posicao == x-1
+
+        #primeira versão
+        #for n in (0..posicao)
+            #if n == posicao
+                #until posicao == x-1
+                #    novo_array[posicao] = array[posicao + 1]
+                #    posicao = posicao + 1
+                #end
+                #novo_array[posicao] = array[posicao + 1]
+            #else
+            #    novo_array[n] = array[n]
+            #end
+        #end
+        
+        #segunda versão
+            for n in (0..x-2)
+                if n >= posicao
                     novo_array[posicao] = array[posicao + 1]
                     posicao = posicao + 1
+                else
+                    novo_array[n] = array[n]
                 end
-            else
-                novo_array[n] = array[n]
             end
-        end  
     end
     return novo_array
 end
